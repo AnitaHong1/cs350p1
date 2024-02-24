bcb2047ece52b7946a952975b4166312b5a615c2
Anita Hong <95871056+AnitaHong1@users.noreply.github.com>
2024-02-24 15:56:20 -0500

    Add files via upload
    first half

---
+++ sysproc.c
@@ -0,0 +1,140 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+//Anita
+int
+sys_exit2(void)
+{
+  int status_arg;
+  if(argint(0, &status_arg) < 0){
+    return -1;
+  }
+
+  cprintf("Exit2. Integer Exit Status within the system call implementation: %d", status_arg);
+  cprintf("\n");
+  exit();
+  
+  return 0;
+}
+
+int
+sys_shutdown(void)
+{
+  outw(0xB004, 0x0|0x2000);
+  outw(0x604, 0x0|0x2000);
+  return 0;
+}
+
+//jy added sys_shutdown2
+int
+sys_shutdown2(void)
+{
+  char *path;
+  if(argstr(0, &path) > 0){
+    cprintf(path);
+    cprintf("\n");
+     outw(0xB004, 0x0|0x2000);
+     outw(0x604, 0x0|0x2000);
+  }
+  return 0;
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int
+sys_uptime2(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
